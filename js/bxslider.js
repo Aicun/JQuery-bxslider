d38872ae139247bd598b82673fa321a3ad9adb39
Aicun <luaicun@gmail.com>
2017-07-24 22:37:32 -0400

    bxSlider plugin

---
+++ js/bxslider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        captions:true,
+        minSlides:2,
+        maxSlides:2,
+        slideWidth:250,
+        slideMargin:5, 
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        adaptiveHeight: true,
+        //mode: 'fade'
+    });
+})
